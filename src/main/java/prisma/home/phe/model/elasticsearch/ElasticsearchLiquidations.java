4fb6c459b4c028f9bf7c0dbcacd5a2ff8d932f2e
Enriquejpc1 <enrique.parada@redb.ee>
2020-07-17 16:22:39 -0300

    Add format date

--- src/main/java/prisma/home/phe/model/elasticsearch/ElasticsearchLiquidations.java
+++ src/main/java/prisma/home/phe/model/elasticsearch/ElasticsearchLiquidations.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -36,7 +37,7 @@ public class ElasticsearchLiquidations implements Serializable {
   @Field(type=FieldType.Text)
   String cuit;
 
-  @Field(type=FieldType.Text)
+  @Field(type=FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis")
   String periodo;
 
   @Field(type=FieldType.Long)
